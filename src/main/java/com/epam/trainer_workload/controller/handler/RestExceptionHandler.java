4981cfcc38595d13493b8759a4f38b5d7e10a0bc
Arman Kaitanov <arman.kaytanov.7@gmail.com>
2024-11-29 17:43:37 +0500

    feat(handler)

---
+++ src/main/java/com/epam/trainer_workload/controller/handler/RestExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.epam.trainer_workload.controller.handler;
+
+import com.epam.trainer_workload.dto.response.ErrorResponseDto;
+import com.epam.trainer_workload.exception.AuthenticationException;
+import com.epam.trainer_workload.exception.ParseTokenException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<ErrorResponseDto> handleInvalidFormatException(InvalidFormatException ex) {
+        return ResponseEntity.badRequest().body(ErrorResponseDto.of(ex.getOriginalMessage()));
+    }
+
+    @ExceptionHandler(ParseTokenException.class)
+    public ResponseEntity<ErrorResponseDto> handleParseTokenException(ParseTokenException e) {
+        return ResponseEntity.badRequest().body(ErrorResponseDto.of(e.getMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDto> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponseDto.of(e.getMessage()));
+    }
+}
