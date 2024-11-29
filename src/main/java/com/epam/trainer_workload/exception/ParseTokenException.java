ab567f5382087df95b5fab9d8487044b4fb35762
Arman Kaitanov <arman.kaytanov.7@gmail.com>
2024-11-29 17:43:30 +0500

    feat(exception)

---
+++ src/main/java/com/epam/trainer_workload/exception/ParseTokenException.java
@@ -0,0 +1,8 @@
+package com.epam.trainer_workload.exception;
+
+public class ParseTokenException extends RuntimeException{
+
+    public ParseTokenException(String message) {
+        super(message);
+    }
+}
