4e3fdea83c766e330e35f0a7ef54d95331efff50
Arman Kaitanov <arman.kaytanov.7@gmail.com>
2024-12-07 13:29:47 +0500

    refactor(dto): refactor TrainerWorkloadUpdateRequestDto: - added Serializable interface implementation - added long serialVersionUID field

--- src/main/java/com/epam/trainer_workload/dto/request/TrainerWorkloadUpdateRequestDto.java
+++ src/main/java/com/epam/trainer_workload/dto/request/TrainerWorkloadUpdateRequestDto.java
@@ -10,11 +10,16 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @Builder
-public class TrainerWorkloadUpdateRequestDto {
+public class TrainerWorkloadUpdateRequestDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 123456789L;
 
     @NotBlank(message = "Trainer username is required")
     private String trainerUsername;
